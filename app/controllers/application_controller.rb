96bed73d856cc2aadc571026a61ddddc5ea214ac
Marco Metz <marco.metz@gmail.com>
2014-09-21 23:53:02 +0200

    mimetypes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 # encoding: utf-8
 
 class ApplicationController < ActionController::Base
-  include ActionController::MimeResponds
-  include ActionController::ImplicitRender
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def self.mimes_for_respond_to
+    [1]
+  end
 end
