1193224a6cf0a8ac108245a6a173b093753dad70
Marco Metz <marco.metz@gmail.com>
2014-09-21 22:32:19 +0200

    deploy not boot

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,23 @@
+# encoding: utf-8
+
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'gounod'
+set :repo_url, 'https://github.com/gounod/crm.git'
+
+set :deploy_to, '/home/gounod/website'
+
+set :scm, :git
+set :bundle_flags, "--deployment --quiet --binstubs"
+
+set :use_sudo, false
+set :rbenv_ruby, '2.1.2'
+set :rvm_type, :user                     # Defaults to: :auto
+set :rvm_ruby_version, '2.1.2'      # Defaults to: 'default'
+
+set :linked_files, %w{config/database.yml settings.yml config/unicorn/production.rb config/unicorn.rb}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
